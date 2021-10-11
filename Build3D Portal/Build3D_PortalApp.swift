9742c8376161ef2d58bffca1400d9e1649dc6fc7
skyeg3 <skyeg3@gmail.com>
2021-10-10 20:46:47 -0700

    Initial Commit

---
+++ Build3D Portal/Build3D_PortalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Build3D_PortalApp.swift
+//  Build3D Portal
+//
+//  Created by Skye Parker on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Build3D_PortalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Build3D Portal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Build3D Portal
+//
+//  Created by Skye Parker on 10/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
